cffcc334b3f76f49b38aa4cef04cce3eda80a3f9
biswanath.saha@mpokket.com <biswanath.saha@mpokket.com>
2024-11-18 21:41:52 +0530

    cahnges

---
+++ LLD/Task Management System/projectManagement/src/main/java/com/affninf/projectManagement/ProjectManagementApplication.java
@@ -0,0 +1,14 @@
+package com.affninf.projectManagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectManagementApplication.class, args);
+		System.out.println("Hello World");
+	}
+
+}
