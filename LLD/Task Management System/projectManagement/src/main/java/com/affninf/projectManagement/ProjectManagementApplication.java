6a2c279cfcc5ffb2bd133ab7d42a1cf5cec5b43c
biswanath.saha@mpokket.com <biswanath.saha@mpokket.com>
2024-12-03 11:28:19 +0530

    added

--- LLD/Task Management System/projectManagement/src/main/java/com/affninf/projectManagement/ProjectManagementApplication.java
+++ LLD/Task Management System/projectManagement/src/main/java/com/affninf/projectManagement/ProjectManagementApplication.java
@@ -2,13 +2,15 @@ package com.affninf.projectManagement;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.stereotype.Component;
 
 @SpringBootApplication
 public class ProjectManagementApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ProjectManagementApplication.class, args);
-		System.out.println("Hello World");
+		System.out.println("Hello");
 	}
 
 }
